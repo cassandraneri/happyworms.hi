56af0f0d7cb486c3e964520fdceebb21dacc44d7
cassandraneri <neric@hawaii.edu>
2022-12-06 12:10:17 -1000

    fixed footer

--- js/script.js
+++ js/script.js
@@ -28,4 +28,29 @@ const myobserver = new IntersectionObserver((entries) => {
     myobserver.observe(section);
   }); 
   
+  const swiper = new Swiper('.swiper', {
+    // Optional parameters
+  slidesPerView: 1,
+          spaceBetween: 30,
+          loop: true,
+          pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+          },
   
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  });
